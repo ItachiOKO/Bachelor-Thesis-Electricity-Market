ac1ec85e0d65636c6adb939c52a18dec545b9b45
ItachiOKO <136023245+ItachiOKO@users.noreply.github.com>
2025-03-10 22:53:05 +0100

    ok

--- config.py
+++ config.py
@@ -1,20 +1,19 @@
 #Data Config
 START_DATE = "2023-01-01" #included
-END_DATE = "2024-01-01" #excluded
+END_DATE = "2023-02-01" #excluded
 PATH_MARKET_DATA = "data\energy-charts_Stromproduktion_und_Börsenstrompreise_in_Deutschland_2023.csv"
 PATH_PRL_DATA = "data\RESULT_OVERVIEW_CAPACITY_MARKET_FCR_2023-01-01_2023-12-31.xlsx"
 SKIPROWS = 2
 
 #Battery Config
-BATTERY_CAPACITY = 5 #MWh
-BATTERY_PRICE = 200000 * BATTERY_CAPACITY #€
-LIFETIME_CYCLES = 5000
-EFFICIENCY = 0.90 
-SPECIFIC_CHARGE_RATE = 2.5 #MW
+BATTERY_CAPACITY = 1 #MWh
+BATTERY_PRICE = 360000 * BATTERY_CAPACITY #€
+LIFETIME_CYCLES = 9000
+EFFICIENCY = 0.86 
+SPECIFIC_CHARGE_RATE = 1 #MW (1MW = 1MWh/1h)
 SPECIFIC_AGING_COST = BATTERY_PRICE / (BATTERY_CAPACITY * LIFETIME_CYCLES) #€/(MWh*Cycle)
 
 #PRL Config
-PRL_PRICE = 1000000 #€/MWh
 PRL_CYCLES_PER_4h = 1/6
 
 #Export Config
@@ -26,7 +25,7 @@ RESULTS_FILE_NAME_PICKLE = f"results_BC-{BATTERY_CAPACITY}MWH_SCR-{SPECIFIC_CHAR
 CELL_NAMES = {
     'date': 'Date', #only date as date-obj
     'market_price': 'Market_price', #€/MWh
-    'prl_price': 'PRL Price', #€/MWh
+    'prl_price': 'GERMANY_SETTLEMENTCAPACITY_PRICE_[EUR/MW]', #€/MWh
     'buy_volume': 'Buy Volume', #MWh charging volume = buy_volume * EFFICIENCY
     'sell_volume': 'Sell Volume', #MWh discharging volume = sell_volume / EFFICIENCY
     'order_cost': 'Net order value', #€
--- result_export.py
+++ result_export.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 def export_results(df, results_file_name_excel, results_file_name_pickle):
     df.to_pickle(results_file_name_pickle)
+    df.index = df.index.tz_localize(None)
     attrs_df = pd.DataFrame(list(df.attrs.items()), columns=['Attribute', 'Value'])
     with pd.ExcelWriter(results_file_name_excel, engine='xlsxwriter') as writer:
         attrs_df.to_excel(writer, sheet_name='Attributes', index=False)
