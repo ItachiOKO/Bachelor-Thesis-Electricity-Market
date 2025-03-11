1e6426907239166f6bb4ad193003cddc83673762
ItachiOKO <136023245+ItachiOKO@users.noreply.github.com>
2025-03-11 01:05:57 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 #Data Config
 START_DATE = "2023-01-01" #included
-END_DATE = "2023-02-01" #excluded
+END_DATE = "2023-02-05" #excluded
 PATH_MARKET_DATA = "data\energy-charts_Stromproduktion_und_Börsenstrompreise_in_Deutschland_2023.csv"
 PATH_PRL_DATA = "data\RESULT_OVERVIEW_CAPACITY_MARKET_FCR_2023-01-01_2023-12-31.xlsx"
 SKIPROWS = 2
