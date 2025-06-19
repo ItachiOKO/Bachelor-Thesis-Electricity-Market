81eee0ffa1897055cd13eeab8bc13a676d8bf154
ItachiOKO <136023245+ItachiOKO@users.noreply.github.com>
2025-06-19 14:50:56 +0200

    fixed prl 0.5MWH/MW

--- model/constraints/prl.py
+++ model/constraints/prl.py
@@ -7,7 +7,7 @@ from config import (
 def add_prl_mode_constraints(model):
     def prl_ub_iv(m, date, quartal):
         iv = (date, quartal)
-        return m.v_PRL_POWER[iv] <= SYSTEM_POWER * m.v_MODE_PRL[iv]
+        return m.v_PRL_POWER[iv] == SYSTEM_POWER * m.v_MODE_PRL[iv]
     model.c_PRL_POWER_UB_IV = pyo.Constraint(model.D4, rule=prl_ub_iv)
 
     def prl_lb_iv(m, date, quartal):
@@ -16,16 +16,21 @@ def add_prl_mode_constraints(model):
     model.c_PRL_POWER_LB_IV = pyo.Constraint(model.D4, rule=prl_lb_iv)
 
 
+import pyomo.environ as pyo
+
 def add_prl_constraints(model):
+    M_SOC = 1
+
     def soc_prl_discharge_buffer(model, t):
-        return model.v_BAT_SOC[t] * BAT_CAPACITY >= 0.5 * model.e_PRL_POWER[t]
+        interval = model.time_to_interval[t]
+        return model.v_BAT_SOC[t] >= 0.5 * model.v_MODE_PRL[interval]
 
     def soc_prl_charge_buffer(model, t):
-        return (1-model.v_BAT_SOC[t]) * BAT_CAPACITY >= 0.5 * model.e_PRL_POWER[t]
+        interval = model.time_to_interval[t]
+        return model.v_BAT_SOC[t] <= 0.5 * model.v_MODE_PRL[interval] + M_SOC * (1 - model.v_MODE_PRL[interval])
 
     model.soc_prl_discharge_buffer = pyo.Constraint(model.T, rule=soc_prl_discharge_buffer)
     model.soc_prl_charge_buffer    = pyo.Constraint(model.T, rule=soc_prl_charge_buffer)
-
 
 
 
